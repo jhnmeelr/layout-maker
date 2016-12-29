fffb773097f91c4fa4637d683393d1fe4932a505
jhnmeelr <jhn.meelr@gmail.com>
2016-12-29 18:34:26 +0200

    assets/images fix

--- client/scripts/assets.js
+++ client/scripts/assets.js
@@ -8,6 +8,9 @@ require('../assets/images/qiwi.png');
 require('../assets/images/visa.png');
 require('../assets/images/webmoney.png');
 require('../assets/images/yandex.png');
+require('../assets/images/sber.png');
+require('../assets/images/alfa.png');
+require('../assets/images/sms.png');
 // fonts
 require('../assets/fonts/roboto-black.ttf');
 require('../assets/fonts/roboto-regular.ttf');
